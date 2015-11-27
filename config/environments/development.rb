eced046b873b25a64a310ef3f619e2b3eacd9cee
Kazuhisa Etoh <etoh.renrakusaki@gmail.com>
2015-11-28 02:19:55 +1100

    change method about mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -20,7 +20,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address => 'smtp.gmail.com',
     :port => 587,
-    :domain => 'gmail.com'
+    :domain => 'gmail.com',
     :user_name => 'mchoco.kmint@gmail.com',
     :password => 'tenpicture',
     :authentication => :plain,
