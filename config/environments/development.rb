b6d2aa39979269cc0f361087890aa1f3a4670471
Kazuhisa Etoh <etoh.renrakusaki@gmail.com>
2015-11-28 02:28:38 +1100

    change method about mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,13 +18,13 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'localhost:3000'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address => 'smtp.gmail.com',
+    :address => "smtp.gmail.com",
     :port => 587,
     :domain => 'gmail.com',
-    :user_name => 'mchoco.kmint@gmail.com',
-    :password => 'tenpicture',
-    :authentication => :plain,
-    :enable_starttls_auto => true
+    :user_name => "mchoco.kmint@gmail.com",
+    :password => "tenpicture",
+    :authentication => 'plain',
+    :enable_starttls_auto => true,
   }
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
