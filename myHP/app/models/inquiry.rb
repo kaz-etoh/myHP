08ea042358f0852e3c57f66ba6118ced9de56204
Kazuhisa Etoh <etoh.renrakusaki@gmail.com>
2015-11-28 01:01:03 +1100

    change method about mailer

--- myHP/app/models/inquiry.rb
+++
@@ -1,9 +0,0 @@
-class Inquiry
-  include ActiveModel::Model
-
-  attr_accessor :name, :email, :message
-
-  validates :name, :presence => {:message => 'Please enter your name'}
-  validates :email, :presence => {:message => 'Please enter your email'}
-
-end
