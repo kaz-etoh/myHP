1162937e2664130935fe3058f39d5438576461c5
Kazuhisa Etoh <etoh.renrakusaki@gmail.com>
2015-11-28 03:40:59 +1100

    change config about gmail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,12 +18,12 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'localhost:3000'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address => "smtp.gmail.com",
-    :port => 587,
-    :domain => 'gmail.com',
-    :user_name => "mchoco.kmint@gmail.com",
-    :password => "tenpicture",
-    :authentication => 'plain',
+    :address              => 'smtp.gmail.com',
+    :port                 => 587,
+    :domain               => 'smtp.gmail.com',
+    :user_name            => 'etoh.renrakusaki@gmail.com',
+    :password             => 'tenpicture',
+    :authentication       => 'plain',
     :enable_starttls_auto => true,
   }
   # Print deprecation notices to the Rails logger.
